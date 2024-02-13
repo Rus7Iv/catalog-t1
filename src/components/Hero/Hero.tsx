4cf114b1502e151aa55818345516df77fa6f491d
Rus7Iv <rusivary@gmail.com>
2024-02-13 21:29:43 +0400

    added hero component

---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,20 @@
+import './Hero.styles.css'
+
+const Hero = () => (
+  <div className='hero'>
+    <h1 className='hero-background'>
+      Goods4you
+    </h1>
+    <div className='hero-description'>
+      <h2 className='hero-title'>
+        Any products from famous brands with worldwide delivery
+      </h2>
+      <span className='hero-text'>
+        We sell smartphones, laptops, clothes, shoes and many other products at low prices
+      </span>
+      {/* button "Go to shoping" */}
+    </div>
+  </div>
+)
+
+export default Hero
