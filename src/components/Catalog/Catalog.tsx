aa3fd0629cca06a49c8b46ddf0756a889ba1b9b9
Rus7Iv <rusivary@gmail.com>
2024-02-13 22:51:23 +0400

    added json-server, products in Catalog

--- src/components/Catalog/Catalog.tsx
+++ src/components/Catalog/Catalog.tsx
@@ -1,11 +1,37 @@
+import { useEffect, useState } from 'react';
 import './Catalog.styles.css'
 
-const Catalog = () => (
-  <div className='catalog'>
-    <h1 className='catalog-title'>
-      Catalog
-    </h1>
-  </div>
-)
+type Product = {
+  id: number;
+  name: string;
+  price: number;
+  image: string;
+  category: string;
+};
 
-export default Catalog
+const Catalog = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+
+  useEffect(() => {
+    fetch('http://localhost:4000/catalog')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+      .catch(error => console.error('Ошибка:', error));
+  }, []);
+
+  return (
+    <div className='catalog'>
+      <h1 className='catalog-title'>Catalog</h1>
+      {products.map(product => (
+        <div key={product.id} className='product'>
+          <img src={'/' + product.image} alt={product.name} />
+          <h2>{product.name}</h2>
+          <p>{product.price}</p>
+          <p>{product.category}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default Catalog;
