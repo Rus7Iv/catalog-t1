cfa38bc6d040fd055cb5fd20962008aef573bb3b
Rus7Iv <rusivary@gmail.com>
2024-02-20 21:30:51 +0400

    added styles in MainPage

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,14 +1,15 @@
 import AboutUs from "../../components/AboutUs/AboutUs"
 import Catalog from "../../components/Catalog/Catalog"
 import Hero from "../../components/Hero/Hero"
+import './MainPage.styles.css'
 
 const MainPage = () => {
   return (
-    <>
+    <div className="main-page">
       <Hero />
       <Catalog />
       <AboutUs />
-    </>
+    </div>
   )
 }
 
