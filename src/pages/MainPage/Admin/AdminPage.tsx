97f00a9b4df77b908b12c40a9c8a980d175ce2ac
Rus7Iv <rusivary@gmail.com>
2024-02-22 23:04:11 +0400

    added AdminPage

---
+++ src/pages/MainPage/Admin/AdminPage.tsx
@@ -0,0 +1,11 @@
+import './AdminPage.styles.css'
+
+const AdminPage = () => {
+  return (
+    <div className="admin-page">
+      <h1>Admin Page</h1>
+    </div>
+  )
+}
+
+export default AdminPage
--- src/router/router.tsx
+++ src/router/router.tsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router-dom"
 import MainPage from "../pages/MainPage/MainPage"
+import AdminPage from "../pages/MainPage/Admin/AdminPage"
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <MainPage />,
+    },
+    {
+        path: "/admin",
+        element: <AdminPage />,
     },
 ])
